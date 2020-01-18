bfd293e58ac44ec989f026a0c93c8f98b7ff6f46
guyschlesinger <guys797@gmail.com>
2020-01-18 22:18:56 +0200

    part 2

--- test/System/buttomUp/copyArrBU.java
+++ test/System/buttomUp/copyArrBU.java
@@ -1,4 +1,62 @@
 package System.buttomUp;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import System.Program;
+
 public class copyArrBU {
+    private Program program= new Program();
+
+    /**
+     * This function will check the minValue function when the input is null
+     */
+    @Test
+    public void minValueWhenNull()
+    {
+        boolean pass = true;
+        try
+        {
+            program.minValue(null);
+        }
+        catch(Exception e)
+        {
+            pass = false;
+        }
+        assertTrue(pass);
+    }
+
+    /**
+     * This function will check the minValue function when the input is valid
+     */
+    @Test
+    public void minValueValidInput()
+    {
+        int [] arr = {4,3,2,1};
+        int value = program.minValue(arr);
+        assertEquals(1,value);
+    }
+
+    /**
+     * This function will check the minValue function when the input is an empty array
+     */
+    @Test
+    public void minValueValidInputEmpty()
+    {
+        boolean pass= true;
+        try
+        {
+            int [] arr = {};
+            int value = program.minValue(arr);
+        }
+        catch(Exception e)
+        {
+            pass = false;
+        }
+        assertTrue(pass);
+
+    }
+
+
 }
